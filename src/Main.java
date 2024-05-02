9b9e0a49af5a8b351eff5453fa27c825d0942dc3
Georgy <balabanov.georgy@mail.com>
2024-05-02 03:39:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 27_376;
+        int bonus = 20;
+        int miles = ticketPrice / bonus;
+
+        System.out.println(miles + " миль");
+    }
+}
